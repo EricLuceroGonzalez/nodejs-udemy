c095e34ce4f51127d9d3db3f0ca278814c23edfe
Eric Lucero Gonzalez <ericlucerogonzalez@gmail.com>
2020-08-03 16:48:12 -0500

    Basic Express set up and parser

--- 01-express/index.js
+++ 01-express/index.js
@@ -1,22 +1,32 @@
-const http = require("http");
-
-// import express
 const express = require("express");
 // run express as function
 const app = express();
 
+// We need to parse the body (chunks), because node doesnt do (body-parser)
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+
 // implement the middleware 'use' that receive al incoming requests
-app.use((req, res, next) => {
+app.use("/add-product", (req, res, next) => {
   console.log("Inside the middleware");
-  next(); //allows to continue to next middleware
+  res.send(
+    '<form action="/product" method="POST"> <input type="text" name="title"></input><button type="submit">Add product </button></form>'
+  );
+  //   next();
 });
 
-app.use((req, res, next) => {
-  console.log("Second middleware");
+app.post("/product", (req, res, next) => {
+  console.log("Product middleware");
+  // get the body:
+  console.log(req.body);
+
+  res.redirect("/");
+  // next(); //allows to continue to next middleware
+});
+
+app.use("/", (req, res, next) => {
+  console.log("Last middleware");
   res.send("<h1>Hello world!, form express</h1>");
-  next(); //allows to continue to next middleware
 });
 
-
-const server = http.createServer(app)
-server.listen(3000)
+app.listen(3000);
