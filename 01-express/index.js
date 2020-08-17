7bdb5c9f9822f970fd9ca43bb53fbb386b87cf3e
Eric Lucero Gonzalez <ericlucerogonzalez@gmail.com>
2020-08-17 14:44:14 -0500

    Mongo DB basic connect

--- 01-express/index.js
+++ 01-express/index.js
@@ -5,9 +5,11 @@ const bodyParser = require("body-parser");
 // run express as function
 const app = express();
 
-const errorController = require('./controllers/errorPage')
+// Import mongoConnect:
+const mongoConnect = require("./util/database");
+const errorController = require("./controllers/errorPage");
 // Import the ejs files
-app.set("view engine", 'ejs');
+app.set("view engine", "ejs");
 app.set("views", "views");
 
 // Import the routes
@@ -17,10 +19,17 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 // Grant access to public folder
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use((req, res, next) => {});
+
 // Use the admin Routes as middleware
 app.use("/admin", adminRoutes.routes);
 app.use(shopRoutes);
 
 // When reach this - no middleware found:
 app.use(errorController.get404);
-app.listen(3000);
+
+mongoConnect((client) => {
+  console.log(client);
+  app.listen(3000);
+});
---
+++ 01-express/util/database.js
@@ -0,0 +1,15 @@
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
+
+const mongoConnect = (callback) => {
+  MongoClient.connect(process.env.DB_TEST)
+    .then((client) => {
+      console.log("Connected!");
+      callback(client);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+module.exports = mongoConnect;
